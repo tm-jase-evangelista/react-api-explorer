5c1fcdca4f2fb5a3c802f2af347182d76ce46e7a
Jason Evangelista <116556575+tm-jase-evangelista@users.noreply.github.com>
2023-09-11 17:50:45 +0800

    add initial fetch hook for api call (#2)

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,4 +1,18 @@
+import { useFetch } from "./utils/useFetch"
+
+const API_PROVIDER_BASE_URL = "https://api.apis.guru/v2"
+
 const App = () => {
+  const { data, loading, error} = useFetch(`${API_PROVIDER_BASE_URL}/adobe.com.json`)
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
+  if (error) {
+    return <div>{error.toString()}</div>
+  }
+
   return (
     <div>
       Hello World!
