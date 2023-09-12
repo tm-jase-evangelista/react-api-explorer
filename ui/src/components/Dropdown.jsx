9850fb67d7d8335a19f27a21e2da9d01260b7afc
Jason Evangelista <jason@thinkingmachin.es>
2023-09-12 07:21:24 +0000

    address lint errors

--- ui/src/components/Dropdown.jsx
+++ ui/src/components/Dropdown.jsx
@@ -1,6 +1,6 @@
-import '../styles/components/Dropdown.css';
+import "../styles/components/Dropdown.css";
 import { useState } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export const Dropdown = ({ data }) => {
   const [isToggled, setToggle] = useState(false);
@@ -8,43 +8,45 @@ export const Dropdown = ({ data }) => {
 
   const handleToggle = () => {
     setToggle(!isToggled);
-  }
+  };
 
   return (
-    <div className={`dropdown ${isToggled ? "toggled": ""}`}>
-      <div
-        className="row row-toggle"
-        onClick={handleToggle}
-      >
+    <div className={`dropdown ${isToggled ? "toggled" : ""}`}>
+      <div className="row row-toggle" onClick={handleToggle}>
         <div className="label">{data.name}</div>
         {isToggled ? (
           <img
-            src={process.env.PUBLIC_URL + '/dropdown/arrow-up.png'}
+            src={process.env.PUBLIC_URL + "/dropdown/arrow-up.png"}
             alt="Arrow Up"
             width="5%"
           />
         ) : (
           <img
-            src={process.env.PUBLIC_URL + '/dropdown/arrow-down.png'}
+            src={process.env.PUBLIC_URL + "/dropdown/arrow-down.png"}
             alt="Arrow Down"
             width="5%"
           />
         )}
       </div>
       {isToggled && (
-      <div>
-        {data && apiList.map((api) => {
-          const logo = data.apis[api].info['x-logo'].url;
-          const title = data.apis[api].info.title;
-          return (
-          <Link key={title} to={`/details/${data.name}/${api}`} className="row row-redirect">
-            <img src={logo} width="5%" />
-            <div className="label">{title}</div>
-          </Link>
-          )
-        })}
-      </div>
+        <div>
+          {data &&
+            apiList.map((api) => {
+              const logo = data.apis[api].info["x-logo"].url;
+              const title = data.apis[api].info.title;
+              return (
+                <Link
+                  key={title}
+                  to={`/details/${data.name}/${api}`}
+                  className="row row-redirect"
+                >
+                  <img src={logo} width="5%" />
+                  <div className="label">{title}</div>
+                </Link>
+              );
+            })}
+        </div>
       )}
     </div>
-  )
-}
+  );
+};
--- ui/src/components/MainPage.jsx
+++ ui/src/components/MainPage.jsx
@@ -1,49 +1,45 @@
-import '../styles/components/MainPage.css';
-import '../styles/global.css';
+import "../styles/components/MainPage.css";
+import "../styles/global.css";
 import { useState } from "react";
-import { Navbar } from './Navbar';
+import { Navbar } from "./Navbar";
 import { useFetchProviders } from "../utils/useFetchProviders";
-import { Dropdown } from './Dropdown';
+import { Dropdown } from "./Dropdown";
 
 export const MainPage = () => {
-  const { data, loading, error} = useFetchProviders();
+  const { data, loading, error } = useFetchProviders();
 
   const [isNavbarToggled, setNavbarToggle] = useState(false);
 
   const handleOnClick = () => {
     setNavbarToggle(!isNavbarToggled);
-  }
+  };
 
   if (loading) {
-    return <div>Loading...</div>
+    return <div>Loading...</div>;
   }
 
   if (error) {
-    return <div>{error.toString()}</div>
+    return <div>{error.toString()}</div>;
   }
 
   return (
     <div className="main-page">
-      {isNavbarToggled &&
+      {isNavbarToggled && (
         <>
-          <div className='overlay' onClick={handleOnClick}/>
+          <div className="overlay" onClick={handleOnClick} />
           <Navbar>
             <div>
-            {data && data.map((apiData) => {
-              return (
-                <Dropdown
-                  key={apiData.name}
-                  data={apiData}
-                />
-              )
-            })}
+              {data &&
+                data.map((apiData) => {
+                  return <Dropdown key={apiData.name} data={apiData} />;
+                })}
             </div>
           </Navbar>
         </>
-      }
-      <button
-        className='btn-primary'
-        onClick={handleOnClick}>Explore web APIs</button>
+      )}
+      <button className="btn-primary" onClick={handleOnClick}>
+        Explore web APIs
+      </button>
     </div>
-  )
-}
+  );
+};
