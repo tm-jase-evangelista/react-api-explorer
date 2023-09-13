e3a43ce5c155104791e994e259f373b153eafd45
Jason Evangelista <jason@thinkingmachin.es>
2023-09-13 03:47:40 +0000

    create hook for fetching and appending more data

--- ui/src/components/Navbar.jsx
+++ ui/src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import "../styles/components/Navbar.css";
+import { useFetchMoreData } from "../utils/useFetchMoreData";
 
 export const Navbar = ({ children }) => {
+  const { fetchMoreData } = useFetchMoreData();
   return (
     <div className="navbar">
       <h1>Select Provider</h1>
       <div>{children}</div>
+      <button onClick={() => fetchMoreData()}>More</button>
     </div>
   );
 };
---
+++ ui/src/utils/useFetchMoreData.js
@@ -0,0 +1,32 @@
+import { useDataContext } from "./DataContext";
+import { fetchProviderInfo } from "./FetchDataHelpers";
+import { ITEMS_PER_PAGE } from "./constants";
+
+export const useFetchMoreData = () => {
+  const { data, setData, listProvider, pageNum, setPageNum } = useDataContext();
+
+  const getNewProviders = (pageNum, listProvider) => {
+    const indexStart = pageNum * ITEMS_PER_PAGE;
+    const indexEnd = indexStart + ITEMS_PER_PAGE;
+    return listProvider.slice(indexStart, indexEnd);
+  };
+
+  const fetchMoreData = () => {
+    const newProviders = getNewProviders(pageNum, listProvider);
+    const promises = newProviders.map((entry) => fetchProviderInfo(entry));
+    Promise.all(promises)
+      .then((res) => {
+        const newData = res.map((item) => ({
+          name: item.config.api_name,
+          apis: item.data.apis,
+        }));
+        const updatedData = data.concat(newData);
+        setData(updatedData);
+        setPageNum(pageNum + 1);
+      })
+      .catch((error) => {
+        console.error("Error fetching provider info:", error);
+      });
+  };
+  return { fetchMoreData };
+};
